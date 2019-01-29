2940625a3bd6bf7e592713290e951d9a0a7b5fd7
Brendlu16 <brendlu16@sps-prosek.cz>
2019-01-29 08:06:44 +0100

    Přidejte soubory projektu.

---
+++ Znamky/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Znamky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
